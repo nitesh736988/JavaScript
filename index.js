77480b4e0ef8e8c56239719d458af858884fa806
Nitesh Kumar Singh <108386117+nitesh736988@users.noreply.github.com>
2025-11-22 15:55:11 +0530

    Data type

---
+++ index.js
@@ -0,0 +1,23 @@
+// *********** WELCOME TO JAVASCRIPT ******************
+
+// const id = Symbol("123")
+// const anotherid = Symbol("123")
+
+// console.log(id===anotherid)
+
+
+// *************Array, objects, Functions *****************
+
+// const heros = ["shaktiman", "naageaj", "doga"]
+// let myObj = {
+//     name: "nitesh",
+//     age: 22,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello World");
+// }
+
+// myFunction()
+
+ 
